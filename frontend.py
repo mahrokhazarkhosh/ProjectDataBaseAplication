07266f040ac7f22de645cd536ea3ae122d15dfec
mahrokhazarkhosh <mahrokh.azarkhosh@outlook.com>
2024-11-05 23:26:38 +0700

    label be set in form

--- frontend.py
+++ frontend.py
@@ -3,4 +3,20 @@ window = Tk()
 window.title("form")
 window.geometry("600x400")
 window.iconbitmap('icon/Mainicon.icon')
+
+
+
+l1 = Label(window, text="Title")
+l1.grid(row=0, column=0)
+
+l2 = Label(window, text="Author")
+l2.grid(row=0, column=2)
+
+l3 = Label(window, text="Year")
+l3.grid(row=1, column=0)
+
+l4 = Label(window, text="ISBN")
+l4.grid(row=1, column=2)
+
+
 window.mainloop()
