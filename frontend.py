c4abde21ff33e079686d5f3eae69335683b69daf
mahrokhazarkhosh <mahrokh.azarkhosh@outlook.com>
2024-11-07 03:52:42 +0700

    i added function and command to view and search and add in my form

--- frontend.py
+++ frontend.py
@@ -5,7 +5,12 @@ window.title("form")
 window.geometry("600x400")
 window.iconbitmap('icon/Mainicon.icon')
 
+def clear_list():
+    list1.delete(0, END)
 
+def fill_list(books):
+    for book in books:
+        list1.insert(END, book)
 
 l1 = Label(window, text="Title")
 l1.grid(row=0, column=0)
@@ -42,13 +47,27 @@ sb1.grid(row=2, column=5, rowspan=20, columnspan=6)
 list1.configure(yscrollcommand=sb1.set)
 sb1.configure(command=list1.yview)
 
-b1 = Button(window, text="View All",width=15,bg="#B4CDED")
+def view_command():
+    clear_list()
+    books = backend.view()
+    fill_list(books)
+
+b1 = Button(window, text="View All",width=15,bg="#B4CDED",command=lambda :view_command())
 b1.grid(row=2, column=50, columnspan=20)
 
-b2 = Button(window, text="Search Entry",width=15,bg="#B4CDED")
+def search_command():
+    clear_list()
+    books = backend.search(title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
+    fill_list(books)
+
+b2 = Button(window, text="Search Entry",width=15,bg="#B4CDED",command=lambda :search_command())
 b2.grid(row=4, column=50, columnspan=20)
 
-b3 = Button(window, text=" Add Entry",width=15,bg="#B4CDED")
+def add_command():
+    backend.insert(title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
+    view_command()
+
+b3 = Button(window, text=" Add Entry",width=15,bg="#B4CDED",command=lambda :add_command())
 b3.grid(row=6, column=50, columnspan=20)
 
 b4 = Button(window, text="Update Selection",width=15,bg="#B4CDED")
@@ -59,4 +78,6 @@ b5.grid(row=10, column=50, columnspan=20)
 
 b6 = Button(window, text="close",width=15,bg="#B4CDED")
 b6.grid(row=12, column=50, columnspan=20)
+
+view_command()
 window.mainloop()
